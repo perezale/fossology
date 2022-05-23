03d4f3d8fce27742e05d1c305cec3c2983fc599b
scanoss-qg <78024084+scanoss-qg@users.noreply.github.com>
2022-05-23 17:57:47 -0300

    Update blacklist_ext.h

--- src/scanoss/agent/src/blacklist_ext.h
+++ src/scanoss/agent/src/blacklist_ext.h
@@ -1,3 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * src/main.c
+ *
+ * A simple SCANOSS client in C for direct file scanning
+ *
+ * Copyright (C) 2018-2020 SCANOSS.COM
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 char *BLACKLISTED_EXTENSIONS[] = {
 "1",
 "2",
